87e3cfdc7e300c9dc00f7f22ec6a2477319fa130
Philip M. Gollucci <pgollucci@p6m7g8.com>
2018-04-21 21:26:26 -0400

    Initial commit

---
+++ init.zsh
@@ -0,0 +1,14 @@
+p6df::modules::perl::version() { echo "0.0.1" }
+p6df::modules::perl::deps()    { 
+	ModuleDeps=(
+	)
+}
+
+p6df::modules::perl::external::brew() {
+}
+
+p6df::modules::perl::init() {
+
+}
+
+p6df::modules::perl::init
