4ce8b4b1f68363d51fb01da04fa332ef5c2b9e04
Philip M. Gollucci <pgollucci@p6m7g8.com>
2018-05-27 21:05:43 -0400

    sync

--- init.zsh
+++ init.zsh
@@ -1,5 +1,5 @@
 p6df::modules::perl::version() { echo "0.0.1" }
-p6df::modules::perl::deps()    { 
+p6df::modules::perl::deps()    {
 	ModuleDeps=(
 	)
 }
@@ -9,17 +9,19 @@ p6df::modules::perl::external::brew() {
 
 p6df::modules::perl::init() {
 
-  p6df::modules::perl::plenv::init
+#  p6df::modules::perl::plenv::init
 }
 
 p6df::modules::perl::plenv::init() {
     [ -n "$DISABLE_ENVS" ] && return
 
-    export PLENV_ROOT=/Users/pgollucci/.local/share/tokuhirom/plenv
-    p6dfz::util::path_if $PLENV_ROOT/bin
+    PLENV_ROOT=/Users/pgollucci/.local/share/tokuhirom/plenv
 
     if [ -x $PLENV_ROOT/bin/plenv ]; then
+      export PLENV_ROOT
       export HAS_PLENV=1
+
+      p6dfz::util::path_if $PLENV_ROOT/bin
       eval "$(plenv init - zsh)"
     fi
 }
