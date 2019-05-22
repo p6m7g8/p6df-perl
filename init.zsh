e8759264c091d9157cbb786fec1fb2713f2753a0
Philip M. Gollucci <pgollucci@p6m7g8.com>
2019-05-21 21:37:42 -0400

    plausible

--- init.zsh
+++ init.zsh
@@ -2,37 +2,55 @@ p6df::modules::perl::version() { echo "0.0.1" }
 p6df::modules::perl::deps()    {
 	ModuleDeps=(
 		p6m7g8/p6perl
+		tokuhirom/plenv
+		tokuhirom/Perl-Build
 	)
-}
-
-p6df::modules::perl::external::brew() {
-
 }
 
 p6df::modules::perl::home::symlink() {
 
-  # XXX: ENV move
+  mkdir -p $P6_DFZ_SRC_DIR/tokuhirom/plenv/plugins
+  ln -fs $P6_DFZ_SRC_DIR/tokuhirom/Perl-Build $P6_DFZ_SRC_DIR/tokuhirom/plenv/plugins/perl-build
+}
+
+p6df::modules::perl::langs() {
+
+  plenv install 5.28.2
+  plenv global 5.28.2
+
+  plenv install-cpanm
+  plenv rehash
+
+  cpanm --self-upgrade  
+
+  cpanm --force Template \
+        LWP::UserAgent \
+        JSON \
+        MIME::Lite \
+        Text::Replace
+
+  plenv rehash
 }
 
 p6df::modules::perl::init() {
 
-    p6df::modules::perl::plenv::init "$P6_DFZ_SRC_DIR"
+  p6df::modules::perl::plenv::init "$P6_DFZ_SRC_DIR"
 }
 
 p6df::modules::perl::plenv::init() {
-    local dir="$1"
+  local dir="$1"
 
-    [ -n "$DISABLE_ENVS" ] && return
+  [ -n "$DISABLE_ENVS" ] && return
 
-    PLENV_ROOT=$dir/tokuhirom/plenv
+  PLENV_ROOT=$dir/tokuhirom/plenv
 
-    if [ -x $PLENV_ROOT/bin/plenv ]; then
-      export PLENV_ROOT
-      export HAS_PLENV=1
+  if [ -x $PLENV_ROOT/bin/plenv ]; then
+    export PLENV_ROOT
+    export HAS_PLENV=1
 
-      p6df::util::path_if $PLENV_ROOT/bin
-      eval "$(plenv init - zsh)"
-    fi
+    p6df::util::path_if $PLENV_ROOT/bin
+    eval "$(plenv init - zsh)"
+  fi
 }
 
 p6df::prompt::perl::line() {
