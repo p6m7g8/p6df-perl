741f91501a2f808b747c5cf95fb0a78438691ff5
Philip M. Gollucci <pgollucci@p6m7g8.com>
2021-03-13 22:03:53 -0500

    chore: the great vscode indent of 2021

--- bin/deps.sh
+++ bin/deps.sh
@@ -2,17 +2,17 @@
 
 main() {
 
-    cpan -T Log::Log4perl \
-	HTTP::Date \
-	YAML 
-    cpan -T CPAN
-    cpan -T Template \
-	LWP::UserAgent \
-	JSON \
-	MIME::Lite \
-	Text::Replace
+	cpan -T Log::Log4perl \
+		HTTP::Date \
+		YAML
+	cpan -T CPAN
+	cpan -T Template \
+		LWP::UserAgent \
+		JSON \
+		MIME::Lite \
+		Text::Replace
 
-    plenv rehash
+	plenv rehash
 }
 
 main "$@"
